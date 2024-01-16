686b534488a93822286048439303701579951cb8
daidai <987380454@qq.com>
2024-01-17 00:28:46 +0800

    选择排序

--- src/algorithm/algorithm.cpp
+++ src/algorithm/algorithm.cpp
@@ -2,14 +2,16 @@
 //
 
 #include "algorithm.h"
-#include "sort/Sort_Bubble.h"
 #include <iostream>
+#include "sort/Sort_Bubble.h"
+#include "sort/Sort_Selection.h"
+
 int main()
 {
-	std::vector<int> array{1, 2, 5, 3, 4};
-	//sort_bubble<int>(array);
-	sort_bullbe_tuning<int>(array);
-
+	std::vector<int> array{1, 2, 5, 3, 9, 7, 4, 8, 6};
+	// sort_bubble<int>(array);
+	// sort_bullbe_tuning<int>(array);
+	sort_selection<int>(array);
 	for (int i = 0; i < array.size(); i++)
 		std::cout << array[i] << " ";
 	return 0;
