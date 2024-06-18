854f29cb32400d84c5db3b81ffb6c000b0e8e022
苏龙(10014903) <SUL-A@glodon.com>
2024-06-18 18:11:04 +0800

    提交

--- src/minishow/IEditor/front_qt/MyEditor.cpp
+++ src/minishow/IEditor/front_qt/MyEditor.cpp
@@ -1,11 +1,25 @@
 #include "MyEditor.h"
 
 MyEditor::MyEditor(QWidget *parent)
-    : QWidget(parent)
+     : QAbstractScrollArea(parent)
+     , m_offset(0, 0)
 {
+    this->setFocusPolicy(Qt::StrongFocus);
+
 }
 
 MyEditor::~MyEditor()
 {
+}
+
+void MyEditor::paintEvent(QPaintEvent *event)
+{
+    QPainter oPainter(viewport());
+
+}
+
+void MyEditor::scrollContentsBy(int dx, int dy)
+{
+
 }
 
--- src/minishow/IEditor/front_qt/MyEditor.h
+++ src/minishow/IEditor/front_qt/MyEditor.h
@@ -2,13 +2,20 @@
 #define MYEDITOR_H
 
 #include <QWidget>
+#include <QAbstractScrollArea>
 
-class MyEditor : public QWidget
+class MyEditor : public QAbstractScrollArea
 {
     Q_OBJECT
 
 public:
     MyEditor(QWidget *parent = nullptr);
     ~MyEditor();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void scrollContentsBy(int dx, int dy) override;
+private:
+    QPoint m_offset;
 };
 #endif // MYEDITOR_H
